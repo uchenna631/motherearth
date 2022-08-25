f2f248adcb2dfe880ba5797cf82d7587e211d06e
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-08-25 16:10:52 +0000

    Pre-deployment: connect environment variables

---
+++ env.py
@@ -0,0 +1,3 @@
+import OPTIONS
+os.environ["DATABASE_URL"]="postgres://jigvcnimkcarzi:2f95d81da33bcda03d4a788310e59c19467289030fbe28763fc49e0e6b1cbba9@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/do8fnc019i8f"
+OS.environ["SECRET_KEY"]="MyDjangoBlogMotherEarthsecret_key111"
